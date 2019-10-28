5e66cfe6dfb26caea71fdf5ea1bcea7e41eb0d42
ivanjimenez <ivan.jimenez18@educa.madrid.org>
2019-10-28 20:04:34 +0100

    .

--- miProyecto1/src/miProyecto1/Clase1.java
+++ miProyecto1/src/miProyecto1/Clase1.java
@@ -5,7 +5,8 @@ public class Clase1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola, Bienvenidos A Entornos");
-		System.out.println("Hola, Bienvenidos A Entornos");
+		System.out.println("Hola, Bienvenidos A Entornos"); 
+		System.out.println("Hola, Bienvenidos A Entornos3"); 
 	}
 
 }
